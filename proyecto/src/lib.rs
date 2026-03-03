4685d4af32ca12bee8b10a0409c743f083e7aa2f
GARRIDO04 <243110158@cuautitlan.tecnm.mx>
2026-03-02 18:31:38 -0600

    Update lib.rs

--- proyecto/src/lib.rs
+++ proyecto/src/lib.rs
@@ -1,18 +1,77 @@
 use anchor_lang::prelude::*;
-
-declare_id!("");
+declare_id!("YgmRSAUmBegew9qkC8h93omVr9NLy8mGwUZ36sxNmug");
 
 #[program]
-mod modulo {
+pub mod playlist {
     use super::*;
-
-    pub fn mi_primer_instruccion(ctx: Context<Saludo>) -> Result<()> {
-        msg!("Mi instruccion, funciona !!! :D");
+    pub fn crear_playlist(context: Context<CrearPlaylist>, nombre: String) -> Result<()> {
+        let playlist = &mut context.accounts.playlist;
+        playlist.nombre = nombre;
+        playlist.owner = context.accounts.owner.key();
+        playlist.canciones = Vec::new();
         Ok(())
     }
+
+    pub fn agregar_cancion(context: Context<AgregarCancion>, cancion: Cancion) -> Result<()> {
+        let playlist = &mut context.accounts.playlist;
+
+        // Validamos que el firmante sea el owner
+        require!(
+            playlist.owner == context.accounts.owner.key(),
+            ProgramError::IllegalOwner
+        );
+
+        playlist.canciones.push(cancion);
+
+        Ok(())
+        }
+    }
+
+#[derive(InitSpace)]
+#[account]
+pub struct Playlist {
+    #[max_len(60)]
+    nombre: String,
+
+    owner: Pubkey,
+
+    #[max_len(10)]
+    canciones: Vec<Cancion>,
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace, PartialEq, Debug)]
+pub struct Cancion {
+    #[max_len(40)]
+    nombre: String,
+
+    #[max_len(40)]
+    artista: String,
+
+    disponible: bool,
+}
 
 #[derive(Accounts)]
-pub struct Saludo {}
+pub struct CrearPlaylist<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        init,
+        payer = owner,
+        space = Playlist::INIT_SPACE + 8,
+        seeds = [b"playlist", owner.key().as_ref()],
+        bump
+    )]
+    pub playlist: Account<'info, Playlist>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AgregarCancion<'info> {
+    #[account(mut, has_one = owner)]
+    pub playlist: Account<'info, Playlist>,
+
+    pub owner: Signer<'info>,
+}
 
